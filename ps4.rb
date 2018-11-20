459f859cf3a406a30baaf941420dbf4fd8f5dca4
ATron789 <albertopavano@gmail.com>
2018-11-20 21:38:35 +0000

    working on adding entries

--- ps4.rb
+++ ps4.rb
@@ -1,13 +1,27 @@
 require 'sequel'
+require 'pry'
 DATABASE = Sequel.connect('sqlite://games.db')
 
 class Ps4 < Sequel::Model(DATABASE[:ps4])
+  def release_date=(date)
+    super Date.strptime(date, '%m/%d/%Y')
+  end
 end
 get '/ps4' do
   @ps4 = Ps4.all
   slim :ps4
 end
+get '/ps4/new' do
+  @ps4_game = Ps4.new
+  slim :new_ps4
+end
 get '/ps4/:id' do
   @ps4_game = Ps4[params[:id]]
   slim :ps4_game
 end
+
+# post '/ps4' do
+#   ps4_game = Ps4.create(params[:ps4_game])
+#   binding.pry
+#   redirect "/ps4/#{ps4_game.id}"
+# end
