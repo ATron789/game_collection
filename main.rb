dd379553f488de1a8259c6e471678ca169b91cd6
ATron789 <albertopavano@gmail.com>
2018-11-21 15:10:44 +0000

    working on adding entries, mismatch error maybe solved

--- main.rb
+++ main.rb
@@ -23,3 +23,23 @@ end
 not_found do
   slim :not_found
 end
+get '/ps4' do
+  @ps4 = Ps4.all
+  slim :ps4
+end
+get '/ps4/new' do
+  @ps4 = Ps4.new
+  slim :new_ps4
+end
+get '/ps4/:id' do
+  @ps4_game = Ps4[params[:id]]
+  slim :ps4_game
+end
+
+post '/ps4' do
+  @new_ps4 = Ps4.new
+  @new_ps4.set(params[:ps4])
+  @new_ps4.save
+  binding.pry
+  redirect "/ps4/#{@new_ps4[:id]}"
+end
