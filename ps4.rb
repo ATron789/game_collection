0a85bcbcb2e96a9bb9d9c7a1f796f1091f24711f
ATron789 <albertopavano@gmail.com>
2018-11-25 19:11:49 +0000

    working on updating info and art cover

--- ps4.rb
+++ ps4.rb
@@ -5,22 +5,34 @@ require 'open-uri'
 
 class Ps4 < Sequel::Model(DATABASE[:ps4])
   def release_date=(date)
-    super Date.strptime(date, "%e %B %Y")
+    Date.strptime(date, "%e/%m/%Y")
+    super
   end
   def release_date_string
     self[:release_date].strftime("%e %B %Y")
   end
-  def info_wiki
-    url_wiki = "https://en.wikipedia.org/wiki/" + self.title.gsub(/ /, "_")
-    parsed_wiki = Nokogiri::HTML.parse(open(url_wiki))
-    game_info = parsed_wiki.css("div.mw-parser-output p")[1].text.gsub("/n", " ")
-    self.update(info: game_info)
+  def release_date_string_num
+    self[:release_date].strftime("%e/%m/%Y")
   end
+
   def before_create
     self.id ||= Ps4.last.id.next
   end
+  def after_create
+    info_wiki
+    set_art_cover
+  end
+
+  private
+  def info_wiki
+    url_wiki = "https://en.wikipedia.org/wiki/" + self.title.gsub(/ /, "_")
+    parsed_wiki = Nokogiri::HTML.parse(open(url_wiki))
+    game_info = parsed_wiki.css("div.mw-parser-output p")[1].text.gsub("\n", " ")
+    self.update(info: game_info)
+  end
+
   def set_art_cover
-    self.update(pic_link: "#{self.title.downcase.split.join}.jpg")
+    self.update(pic_link: "#{self.title.downcase.split.join.gsub(/[\W\s]/, "")}.jpg")
   end
 end
 
--- spec/ps4_spec.rb
+++ spec/ps4_spec.rb
@@ -5,18 +5,29 @@ describe Ps4 do
   it 'shows the date' do
     expect(fo4.release_date_string).to eq "10 November 2015"
   end
+
   context 'new entry' do
-    let (:batman) {Ps4.create(title: "Batman")}
-    after(:each) do
-      Ps4.where(title: "Batman").destroy
+
+    let (:batman) do
+      batman = Ps4.new
+      batman.set(title: "Batman: Arkham Knight")
+      batman.save
     end
+    after(:each) do
+      Ps4.where(title: "Batman: Arkham Knight").destroy
+    end
+
     it 'the next entry id is the next value of the last entry' do
       expect(batman.id).to eq fo4.id.next
     end
-    it 'pic_link setter finds the cover in the folder' do
-      fo4.set_art_cover
-      binding.pry
-      expect(fo4.pic_link).to eq "#{fo4.title.downcase.split.join}.jpg"
+    it 'pic_link from wikipedia after creation' do
+      expect(batman.pic_link).to eq "batmanarkhamknight.jpg"
+    end
+    it 'gets the info from wikipedia after creation' do
+      url_wiki = "https://en.wikipedia.org/wiki/" + batman.title.gsub(/ /, "_")
+      parsed_wiki = Nokogiri::HTML.parse(open(url_wiki))
+      game_info = parsed_wiki.css("div.mw-parser-output p")[1].text.gsub("\n", " ")
+      expect(batman.info).to eq game_info
     end
   end
 
